ab9a2c7cc6ce1e3c791085da2d865d4b84e1521f
William Shields <48608517+thenaturalwill@users.noreply.github.com>
2021-02-16 19:12:00 -0800

    100 days of Code: DAY 4

---
+++ Chapter-3/bean-counter/counter.js
@@ -0,0 +1,28 @@
+/*
+Write a function that counts all of the B's in a string
+Then rewrite it to count a giving char on input.
+*/
+
+function countBs(toTest) {
+  let count = 0;
+  for (var i = 0; i < toTest.length; i++) {
+    if (toTest.charAt([i]) == "B") {
+      count += 1;
+    }
+  }
+  return count;
+}
+console.log(countBs("Bahh Bahh Black Sheep"));
+
+function countChar(stringToTest, charToFind) {
+  let count = 0;
+  for (var i = 0; i < stringToTest.length; i++) {
+    if (stringToTest.charAt([i]) === charToFind) {
+      count += 1;
+    }
+  }
+  return alert(
+    "There are " + count + " " + charToFind + "'s' in: \n" + stringToTest
+  );
+}
+countChar(prompt("Enter a String:"), prompt("What Char Should I count??"));
