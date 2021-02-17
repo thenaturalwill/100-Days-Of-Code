40f7827613ea7867f21485a5dbc0c05452e2ed79
William Shields <48608517+thenaturalwill@users.noreply.github.com>
2021-02-17 14:54:00 -0800

    100 Days of Code -Day 4 & 5
    Added a front end to the bean counter, and allowed it to search for whole words. Still need to figure out a way to distinguish punctuation marks

--- Chapter-3/bean-counter/counter.js
+++ Chapter-3/bean-counter/counter.js
@@ -14,14 +14,31 @@ function countBs(toTest) {
 }
 console.log(countBs("Bahh Bahh Black Sheep"));
 
+// Logic to search and Input Field to find Givin Text
 function countChar(stringToTest, charToFind) {
   let count = 0;
-  for (var i = 0; i < stringToTest.length; i++) {
-    if (stringToTest.charAt([i]) === charToFind) {
-      count += 1;
+  let string = document.getElementById(stringToTest).value;
+  let char = document.getElementById(charToFind).value;
+
+  if (char.length < 2 && char.length > 0) {
+    for (var i = 0; i < string.length; i++) {
+      if (string.charAt([i]) === char) {
+        count += 1;
+      }
     }
   }
-  return alert(`There are ${count} ${charToFind}'s' in: ${stringToTest}`);
+  // This is working, but it will not check for punctuation marks yet :(
+  else if (char.length >= 2) {
+    stringArrary = string.split(" ");
+    console.log(stringArrary.length);
+    console.log(stringArrary);
+    for (var i = 0; i <= stringArrary.length; i++) {
+      if (stringArrary[i] == char) {
+        count += 1;
+      }
+    }
+  } else {
+    return alert("Please enter at least ONE character to search for.");
+  }
+  return alert(`${char} appears ${count} times in: \n\n ${string}`);
 }
-
-countChar(prompt("Enter a String:"), prompt("What Char Should I count??"));
