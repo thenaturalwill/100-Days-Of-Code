751da738724cec304e0826a44cebc24084e38aeb
William Shields <48608517+thenaturalwill@users.noreply.github.com>
2021-02-22 15:51:24 -0800

    Updated Front End
    Day 10 of 100 days of code, finished up the css now I am moving on to the javascript

--- Calculator/js/calculate.js
+++ Calculator/js/calculate.js
@@ -1,2 +1,21 @@
 // Work - In Progress
 // Scope is to create a working Calculator with HTML-CSS-JS
+
+let toDisplayCalc = true;
+let displayTotal = "0";
+let displayNumbers = "0";
+
+function openCalc() {
+  const DISPLAY = document.getElementById("calc-display");
+  const BODY = document.getElementById("calc-body");
+
+  if (toDisplayCalc == false) {
+    DISPLAY.style.display = "block";
+    BODY.style.display = "grid";
+    toDisplayCalc = true;
+  } else {
+    DISPLAY.style.display = "none";
+    BODY.style.display = "none";
+    toDisplayCalc = false;
+  }
+}
