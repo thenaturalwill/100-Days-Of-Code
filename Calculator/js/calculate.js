bea69d65da2f1f321b182bae87b337b7f8d7f204
William Shields <48608517+thenaturalwill@users.noreply.github.com>
2021-02-23 21:00:39 -0800

    Updated the JavaScript - Day 11

--- Calculator/js/calculate.js
+++ Calculator/js/calculate.js
@@ -1,14 +1,15 @@
 // Work - In Progress
 // Scope is to create a working Calculator with HTML-CSS-JS
-
+let runningTotalDisplay = document.getElementById("running_total");
+let totalDisplay = document.getElementById("total_calculated");
 let toDisplayCalc = true;
-let displayTotal = "0";
-let displayNumbers = "0";
+let runningTotal = "0";
+let total = 0;
 
+// Handeling of Window Event - Extra Feature
 function openCalc() {
   const DISPLAY = document.getElementById("calc-display");
   const BODY = document.getElementById("calc-body");
-
   if (toDisplayCalc == false) {
     DISPLAY.style.display = "block";
     BODY.style.display = "grid";
@@ -19,3 +20,36 @@ function openCalc() {
     toDisplayCalc = false;
   }
 }
+
+//Calculations
+function calculate() {
+  //handeling equals
+  if (runningTotalDisplay.innerHTML.length <= 24) {
+    runningTotalDisplay.innerHTML = runningTotal;
+  } else {
+    runningTotalDisplay.innerHTML = total.toString;
+  }
+  totalDisplay.innerHTML = parseFloat(runningTotal);
+}
+
+// Div Buttons
+function autoclear() {}
+
+function clear() {}
+
+function sign() {}
+
+function buttonPress(button) {
+  if (runningTotal == "0") {
+    runningTotal = button;
+    runningTotalDisplay.innerHTML = runningTotal;
+  } else if (runningTotalDisplay.innerHTML.length <= 24) {
+    runningTotal += button;
+    runningTotalDisplay.innerHTML = runningTotal;
+  } else {
+    runningTotal = total.toString();
+    runningTotalDisplay.innerHTML = total.toString();
+  }
+}
+
+//Keyboard Handeling
